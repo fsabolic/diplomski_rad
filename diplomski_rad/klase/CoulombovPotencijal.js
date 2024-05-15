cf4b0acc66dbcbb8de8dc70fa5ece43ad633307a
fsabolic <fsabolic@foi.hr>
2024-05-15 18:55:18 +0200

    Omogućeno isključivanje potencijala pri približavanju čestica

--- diplomski_rad/klase/CoulombovPotencijal.js
+++ diplomski_rad/klase/CoulombovPotencijal.js
@@ -1,19 +1,26 @@
 class CoulombovPotencijal {
   constructor(k, rVektor, naboj = 1, potencijalUdaljenosti = 2) {
     this.k = k;
+    this.pom = k;
     this.r = rVektor;
     this.naboj = naboj;
     this.potencijalUdaljenosti = potencijalUdaljenosti;
   }
 
-  F(materijalnaTocka) {
+  F(materijalnaTocka, maxUdaljenost = -1) {
     let rRez = this.r.oduzmi(materijalnaTocka.r);
 
     let udaljenost = this.r.udaljenost(materijalnaTocka.r);
 
-    return rRez
-      .pomnoziSkalarom(this.naboj * this.k)
-      .pomnoziSkalarom(1 / udaljenost ** this.potencijalUdaljenosti);
+    if (maxUdaljenost > -1 && maxUdaljenost > udaljenost) {
+      this.k = 0;
+    } else {
+      //this.k = this.pom;
+    }
+
+    let mnozitelj =
+      (this.naboj * this.k) / udaljenost ** this.potencijalUdaljenosti;
+    return rRez.pomnoziSkalarom(mnozitelj);
   }
 
   iscrtaj(gks) {
