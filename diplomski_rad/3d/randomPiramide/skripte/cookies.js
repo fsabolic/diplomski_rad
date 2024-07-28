cbc78c0be418980d39753deec2a7c210a006dbb0
Fran Sabolić <fransabolicm8@gmail.com>
2024-07-29 00:35:24 +0200

    Dodan localstorage umjesto cookies (treba napraviti preimenovanje)

--- diplomski_rad/3d/randomPiramide/skripte/cookies.js
+++ diplomski_rad/3d/randomPiramide/skripte/cookies.js
@@ -2,10 +2,16 @@ function dohvatiPostavkeCookie() {
   let cookieString = document.cookie.valueOf("postavke-webgpu-sim");
   if (!cookieString) return null;
   let cookiePostavke = JSON.parse(cookieString.split("=")[1]);
-  return cookiePostavke;
+
+  cookiePostavke = window.localStorage.getItem("postavke-webgpu-sim");
+
+  if (!cookieString) return null;
+
+  return JSON.parse(cookiePostavke);
 }
 
 function postaviPostavkeCookie() {
   document.cookie =
     "postavke-webgpu-sim=" + JSON.stringify(postavke) + "; path=/";
+  window.localStorage.setItem("postavke-webgpu-sim", JSON.stringify(postavke));
 }
