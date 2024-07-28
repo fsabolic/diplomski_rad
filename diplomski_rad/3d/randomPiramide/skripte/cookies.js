9e508b34661578a4a7390a6d913a453c2be4407d
Fran Sabolić <fransabolicm8@gmail.com>
2024-07-29 01:12:54 +0200

    Prepravljen rad za localstorageom

--- diplomski_rad/3d/randomPiramide/skripte/cookies.js
+++ diplomski_rad/3d/randomPiramide/skripte/cookies.js
@@ -1,11 +1,9 @@
 function dohvatiPostavkeCookie() {
   let cookieString = document.cookie.valueOf("postavke-webgpu-sim");
-  if (!cookieString) return null;
-  let cookiePostavke = JSON.parse(cookieString.split("=")[1]);
-
-  cookieString = window.localStorage.getItem("postavke-webgpu-sim");
-
-  if (!cookieString) return null;
+  if (!cookieString) {
+    cookieString = window.localStorage.getItem("postavke-webgpu-sim");
+    if (!cookieString) return null;
+  }
 
   return JSON.parse(cookieString);
 }
